3e1aa106635cdd28a0f03548dddf124fa3afc162
Student <student@b528b-7.intranet.cs.upt.ro>
2026-04-15 17:39:54 +0300

    inceput

--- main.c
+++ main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
-int main(void)
+#define inspSize 100
+#define catSize 50
+#define desSize 100
+
+typedef struct
 {
-    printf("Hello, World!\n");
+    float latitudine;
+    float longitudine;
+}GPS;
+
+typedef struct
+{
+    int id;
+    char inspectorName[inspSize];
+    GPS gps;
+    char category [catSize];
+    int severity;
+    time_t timestamp;
+    char description[desSize];
+}REPORT;
+
+
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        char text[]="Too few arguments";
+        write(2,text,sizeof(text));
+        return 0;
+    }
+    if (strcmp(argv[2],"--role")!=0)
+    {
+        char text[]="Wrong syntax";
+        write(2,text,sizeof(text));
+        return 0;
+    }
     return 0;
 }
